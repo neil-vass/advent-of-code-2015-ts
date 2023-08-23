77cb0fdcd7590dd5e99059204e03ea6e47a787e0
Neil Vass <neil.vass2@coop.co.uk>
2023-08-23 19:52:12 +0100

    Use describe/it test syntax

--- test/day01.test.ts
+++ test/day01.test.ts
@@ -1,14 +1,16 @@
-import {expect, test} from "vitest";
-const day01 = require("../src/day01.js")
+import {expect, test, describe, it} from "vitest";
+const day01 = require("../src/day01")
 
+describe("Vitest setup", () => {
+    it("Makes simple comparisons", () => {
+        expect(1).toBe(1);
+    });
 
-test("Trivial comparison works", () => {
-    expect(1).toBe(1);
+    it("Can call src files", () => {
+        expect(day01.callMe()).toBe("Hi!");
+    });
 });
 
 
-test("calling src files works", () => {
-    expect(day01.callMe()).toBe("Hi!");
-});
 
 
