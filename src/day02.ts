bfe14a63fd934563ca00a42271b5f724dad3d07e
Neil Vass <neil.vass2@coop.co.uk>
2023-09-04 15:07:21 +0100

    Deal with top-level async by using void main() instead of anonymous async IIFE

--- src/day02.ts
+++ src/day02.ts
@@ -16,20 +16,19 @@ export function requiredWrapping(length: number, width: number, height: number) 
     return sides.reduce((acc, val) => acc + 2*val, smallestSide);
 }
 
+async function main() {
+    const data = fetchData("src/data/day02.txt");
+    // TODO: is there a nice .reduce() syntax I can use?
+    // TODO: Python accepts generators easily, to pipeline things like that.
+    // TODO: Don't know if this being an async generator complicates things...
+    let total = 0;
+    for await (const dimensions of data) {
+        // TODO: How can I ...spread the array in here as arguments?
+        total += requiredWrapping(dimensions[0],dimensions[1],dimensions[2]);
+    }
+    console.log(total);
+}
+
 if (require.main === module) {
-    // TODO: Can't have a top-level await without changing some settings -
-    // TODO: here's a hacky "pretend it's async" thing until I change those.
-    (async () => {
-        const data = fetchData("src/data/day02.txt");
-        // TODO: is there a nice .reduce() syntax I can use?
-        // TODO: Python accepts generators easily, to pipeline things like that.
-        // TODO: Don't know if this being an async generator complicates things...
-        let total = 0;
-        for await (const dimensions of data) {
-            // TODO: How can I ...spread the array in here as arguments?
-            total += requiredWrapping(dimensions[0],dimensions[1],dimensions[2]);
-        }
-        console.log(total);
-    })();
-    console.log();
+    void main();
 }
