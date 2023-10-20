032c4d967916d2d1122583cafac8047b659a4586
Neil Vass <neil.vass2@coop.co.uk>
2023-10-20 22:25:12 +0100

    Working on permutations in Day 9

--- src/day09.ts
+++ src/day09.ts
@@ -1,6 +1,70 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
 
+export class DistanceTable {
+    private readonly cities = new Set<string>();
+    private readonly distances = new Map<string, number>();
+
+    store(from: string, to: string, distance: number) {
+        this.cities.add(from);
+        this.cities.add(to);
+        this.distances.set(`${from}-${to}`, distance);
+    }
+
+    allCities() {
+        return [...this.cities];
+    }
+
+    distance(from: string, to: string) {
+        if(this.distances.has(`${from}-${to}`))
+            return this.distances.get(`${from}-${to}`);
+        if(this.distances.has(`${to}-${from}`))
+            return this.distances.get(`${to}-${from}`);
+        throw new Error(`Don't know distance between ${from} and ${to}`);
+    }
+}
+
+export function permutations<T>(arr: T[]) {
+    return new Sequence(generatePermutations(arr));
+}
+
+
+// Heap's algorithm: a "decrease and conquer" method.
+// Operates on 'k' elements of the array at each step
+// (k starts at array's length and decreases by one each step).
+function* generatePermutations<T>(arr: T[], k: number = arr.length): Iterable<T[]> {
+    if (k === 1) {
+        yield [...arr];
+    } else {
+        for (let i = 0; i < k; i++) {
+            yield* generatePermutations(arr, k - 1);
+
+            if (k % 2 === 0) {
+                [arr[i], arr[k-1]] = [arr[k-1], arr[i]];
+            }
+            else {
+                [arr[0], arr[k-1]] = [arr[k-1], arr[0]];
+            }
+        }
+    }
+}
+
+
+export async function calculateRoutes(input: Sequence<string>) {
+    const distanceTable = new DistanceTable();
+
+    for await (const s of input) {
+        const m = s.match(/^(\w+) to (\w+) = (\d+)$/);
+        if (!m) throw new Error(`Unexpected line: "${s}"`);
+
+        const [from, to, distance] = m.slice(1);
+        distanceTable.store(from, to, +distance);
+    }
+
+
+}
+
+
 export function fn(filepath: string) {
     return "Hello, World!";
 }
@@ -9,4 +73,10 @@ export function fn(filepath: string) {
 if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = "./src/data/day09.txt";
     console.log(fn(filepath));
+
+    for (const p of generatePermutations(["a", "b"])) {
+        console.log(JSON.stringify(p));
+    }
+
+
 }
--- test/day09.test.ts
+++ test/day09.test.ts
@@ -1,10 +1,74 @@
-import {expect, describe, it} from "vitest";
+import {expect, describe, it, beforeEach} from "vitest";
 import * as day09 from "../src/day09";
+import {Sequence} from "../src/sequence";
+import {DistanceTable} from "../src/day09";
 
-const testFilename = "./test/data/day09.txt"
+describe("DistanceTable class", () => {
+    it("Remembers entries", () => {
+        const dt = new DistanceTable();
+        dt.store("London", "Dublin", 464);
+        dt.store("London", "Belfast", 518);
+        dt.store("Dublin", "Belfast", 141);
 
-describe("#fn", () => {
-    it("runs", () => {
-        expect(day09.fn(testFilename)).toBe("a real test");
+        expect(dt.allCities().sort()).toStrictEqual(["London", "Belfast", "Dublin"].sort());
+        expect(dt.distance("London", "Dublin")).toBe(464);
+        expect(dt.distance("Dublin", "London")).toBe(464);
+    });
+
+
+});
+
+describe("#calculateRoutes", () => {
+
+    let sampleInput: Sequence<string>;
+
+    beforeEach(()=> {
+        sampleInput = new Sequence([
+            "London to Dublin = 464",
+            "London to Belfast = 518",
+            "Dublin to Belfast = 141",
+        ]);
+    });
+
+    it("Finds distances for all possible route permutations", async () => {
+        const results = await day09.calculateRoutes(sampleInput);
+        expect(results).toStrictEqual([
+            { route: "Dublin -> London -> Belfast", distance: 982 },
+            { route: "London -> Dublin -> Belfast", distance: 605 },
+            { route: "London -> Belfast -> Dublin", distance: 659 },
+            { route: "Dublin -> Belfast -> London", distance: 659 },
+            { route: "Belfast -> Dublin -> London", distance: 605 },
+            { route: "Belfast -> London -> Dublin", distance: 982 }
+        ])
+    });
+});
+
+describe("#permutations", () => {
+
+    it("Returns 1 permutation for 1-element array", async () => {
+        const allPermutations = await day09.permutations(["A"]).toArray();
+        expect(allPermutations).toStrictEqual([["A"]]);
+    });
+
+    it("Returns 2 permutation for 2-element array", async () => {
+        const allPermutations = await day09.permutations(["a", "b"]).toArray();
+        expect(allPermutations.length).toBe(2);
+        expect(allPermutations).toEqual(expect.arrayContaining([
+            ["a", "b"],
+            ["b", "a"]
+        ]));
+    })
+
+    it("Returns 6 permutation for 3-element array", async () => {
+        const allPermutations = await day09.permutations([1,2,3]).toArray();
+        expect(allPermutations.length).toBe(6);
+        expect(allPermutations).toEqual(expect.arrayContaining([
+            [1,2,3],
+            [2,1,3],
+            [3,1,2],
+            [1,3,2],
+            [2,3,1],
+            [3,2,1]
+        ]));
     });
 });
