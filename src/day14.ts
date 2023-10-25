213dadf0686d01cd0206220e4381cd3c011a68e8
Neil Vass <neil.vass2@coop.co.uk>
2023-10-25 17:39:51 +0100

    Day 14 complete

--- src/day14.ts
+++ src/day14.ts
@@ -1,6 +1,7 @@
 import {linesFromFile} from "./helpers.js";
 import {Sequence} from "./sequence.js";
 
+
 export class Reindeer {
     constructor(
         private readonly speed: number,
@@ -20,6 +21,24 @@ export class Reindeer {
     }
 }
 
+export class ReindeerRacer {
+    constructor(readonly competitors: Array<Reindeer>) {}
+
+    scoresAfter(duration: number) {
+        const tracker = this.competitors.map(c => ({ reindeer: c, pos: 0, score: 0}));
+
+        for (let t=1; t <= duration; t++) {
+            tracker.forEach(c => c.pos = c.reindeer.distanceAfter(t));
+            const leadPosition = Math.max(...tracker.map(c => c.pos));
+            tracker.forEach(c => { if (c.pos === leadPosition) c.score += 1 })
+        }
+
+        return tracker.map(c => c.score);
+    }
+}
+
+
+
 export function reindeerFromDescription(line: string) : Reindeer {
     const m = line.match(/^\w+ can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds.$/);
     if (!m) throw new Error(`Unexpected line format: "${line}"`);
@@ -32,14 +51,17 @@ export async function findWinningDistance(raceDuration: number, reindeerDescript
     return Sequence.max(distances);
 }
 
-export function fn(filepath: string) {
-    return "Hello, World!";
+export async function raceReindeerPart2(raceDuration: number, reindeerDescriptions: Sequence<string>) {
+    const competitors = await reindeerDescriptions.map(rd => reindeerFromDescription(rd)).toArray();
+    const racer = new ReindeerRacer(competitors);
+    return Math.max(...racer.scoresAfter(raceDuration));
 }
+
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const raceDuration = 2503;
     const reindeerDescriptions = linesFromFile("./src/data/day14.txt");
-    console.log(await findWinningDistance(raceDuration, reindeerDescriptions));
+    console.log(await raceReindeerPart2(raceDuration, reindeerDescriptions));
 }
 
--- test/day14.test.ts
+++ test/day14.test.ts
@@ -1,6 +1,6 @@
 import {expect, describe, it} from "vitest";
 import * as day14 from "../src/day14";
-import {findWinningDistance} from "../src/day14";
+import {findWinningDistance, Reindeer} from "../src/day14";
 import {Sequence} from "../src/sequence";
 
 const testFilename = "./test/data/day14.txt"
@@ -31,5 +31,30 @@ describe("#findWinningDistance", () => {
             "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
         ]);
         expect(await day14.findWinningDistance(1000, reindeerDescriptions)).toBe(1120);
+    });
+});
+
+describe("#ReindeerRacer", () => {
+    it("Reindeer rack up point throughout the race duration, depending on who's in the lead.", async () => {
+        const competitors = [
+            new Reindeer(14, 10, 127), // Comet
+            new Reindeer(16, 11, 162)  // Dancer
+        ];
+
+        const racer = new day14.ReindeerRacer(competitors);
+        expect(racer.scoresAfter(1)).toStrictEqual([0, 1]);
+        expect(racer.scoresAfter(140)).toStrictEqual([1, 139]);
+        expect(racer.scoresAfter(1000)).toStrictEqual([312, 689]);
+
+    });
+});
+
+describe("#raceReindeerPart2", () => {
+    it("Gives the same result as puzzle example.", async () => {
+        const reindeerDescriptions = new Sequence([
+            "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+            "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
+        ]);
+        expect(await day14.raceReindeerPart2(1000, reindeerDescriptions)).toBe(689);
     });
 });
