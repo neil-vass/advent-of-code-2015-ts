42222a2bbe4e1222140104c72ffc76cbe0fd0b9e
Neil Vass <neil.vass2@coop.co.uk>
2023-11-11 17:29:07 +0000

    Day 16 complete

--- src/day16.ts
+++ src/day16.ts
@@ -1,20 +1,72 @@
-import {Sue} from "./suspectFacts.js";
+import {linesFromFile} from "./helpers.js";
+
+// Let's try currying! https://javascript.info/currying-partials
+function comparator(fn: (a: number, b: number) => boolean) {
+    return function (target: number) {
+        return function (input: number) {
+            return fn(input, target);
+        }
+    }
+}
+
+const equalTo = comparator((a, b) => a === b);
+const greaterThan = comparator((a, b) => a > b);
+const lessThan = comparator((a, b) => a < b);
+
+const suspectFacts = new Map([
+    ["children", equalTo(3)],
+    ["cats", greaterThan(7)],
+    ["samoyeds", equalTo(2)],
+    ["pomeranians", lessThan(3)],
+    ["akitas", equalTo(0)],
+    ["vizslas", equalTo(0)],
+    ["goldfish", lessThan(5)],
+    ["trees", greaterThan(3)],
+    ["cars", equalTo(2)],
+    ["perfumes", equalTo(1)],
+]);
+
+export class Sue {
+    public readonly id;
+    private readonly knownFacts;
+
+    constructor(id: string, knownFacts: [string, number][]) {
+        this.id = id;
+        this.knownFacts = new Map(knownFacts);
+    }
+
+    fitsTheFacts() {
+        for (const [thing, meetsRequirement] of suspectFacts) {
+            const value = this.knownFacts.get(thing);
+            if (value === undefined) continue;
+            if (!meetsRequirement(value)) return false;
+        }
+        return true;
+    }
+}
 
 export function sueFromDescription(description: string) {
-    //"Sue 405: akitas: 1, vizslas: 6, perfumes: 6"
     const m = description.match(/^Sue (\d+): (.+)$/);
     if (!m) throw new Error(`Unexpected line format ${description}`);
     const [, id, factsStr] = m;
 
-    factsStr.split(',').forEach()
-    return new Sue(1, []);
+    const facts = factsStr.split(',').map(f =>
+    {
+        const m = f.match(/(\w+): (\d+)/);
+        if (!m) throw new Error(`Unexpected format ${f}`);
+        const [, name, num] = m;
+        return [name, +num] as [string, number];
+    })
+    return new Sue(id, facts);
 }
-export function fn(filepath: string) {
-    return "Hello, World!";
-}
+
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
-    const filepath = "./src/data/day16.txt";
-    console.log(fn(filepath));
+    const lines = linesFromFile("./src/data/day16.txt");
+    for await (const sue of lines.map(ln => sueFromDescription(ln))) {
+        if (sue.fitsTheFacts()) {
+            console.log(sue.id);
+        }
+    }
 }
--- test/day16.test.ts
+++ test/day16.test.ts
@@ -1,26 +1,29 @@
 import {describe, expect, it, vi} from "vitest";
-import * as day16 from "../src/day16";
-
-import {Sue} from "../src/suspectFacts";
+import {Sue, sueFromDescription} from "../src/day16";
 
 describe("#fitsTheFacts", () => {
     it("True if the known facts about Sue fits all the facts", () => {
-        const sue = new Sue(1, [["children", 3], ["cats", 7]]);
+        const sue = new Sue(1, [["children", 3], ["samoyeds", 2]]);
         expect(sue.fitsTheFacts()).toBeTruthy();
     });
 
     it("False if any known facts about Sue disagrees with a fact", () => {
-        const sue = new Sue(1, [["children", 3], ["cats", 1]]);
+        const sue = new Sue(1, [["children", 3], ["samoyeds", 1]]);
         expect(sue.fitsTheFacts()).toBeFalsy();
+    });
+
+    it("Must have greater than the 7 cats given in the suspect facts", () => {
+        const sue = new Sue(1, [["children", 3], ["cats", 7]]);
+        expect(sue.fitsTheFacts()).toBeFalsy();
+
+        const sue2 = new Sue(1, [["children", 3], ["cats", 10]]);
+        expect(sue2.fitsTheFacts()).toBeTruthy();
     });
 });
 
 describe("#sueFromDescription", () => {
     it("Creates Sue with given facts", async () => {
         const description = "Sue 405: akitas: 1, vizslas: 6, perfumes: 6";
-        vi.mock("../src/suspectFacts");
-        day16.sueFromDescription(description);
-        expect(Sue).toHaveBeenCalledOnce();
-
+        sueFromDescription(description);
     });
 });
